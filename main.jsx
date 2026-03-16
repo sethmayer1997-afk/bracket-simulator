4de1d231d1853b0027ed6c50ae5c6bd2c92f99c1
sethmayer1997-afk <seth.mayer1997@gmail.com>
2026-03-16 11:10:34 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+
+inject()
+injectSpeedInsights()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
